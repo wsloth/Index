fd93bbccd76352d1219e7b7bf42198307e150612
Wessel Loth <wessel@loth.io>
2021-01-15 23:40:03 +0100

    Add nicely designed collapsible app header

--- lib/views/pages/front-page.dart
+++ lib/views/pages/front-page.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:index/widgets/article.dart';
+import 'package:index/widgets/separator.dart';
 import 'package:timeago/timeago.dart' as timeago;
+import 'package:customizable_space_bar/customizable_space_bar.dart';
 
 import 'package:index/widgets/url.dart';
 import 'package:index/widgets/error.dart';
@@ -29,32 +30,28 @@ class _FrontPageState extends State<FrontPage> {
   Widget _buildRow(ArticleModel article) {
     return ListTile(
       contentPadding: EdgeInsets.fromLTRB(0, 5, 0, 5),
-
       title: Text(
         article.title,
         style: _biggerFont,
       ),
-
       subtitle: Container(
         margin: EdgeInsets.only(top: 8),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.baseline,
-          children: [
-            Container(child: getArticleScoreStylizedText(context, article.score), margin: EdgeInsets.only(right: 8)),
-            Container(child: Text(timeago.format(article.time)), margin: EdgeInsets.only(right: 8)),
-            getReadableUrlWidget(article.url),
-          ]
-        ),
+        child: Row(crossAxisAlignment: CrossAxisAlignment.baseline, children: [
+          Container(
+              child: getArticleScoreStylizedText(context, article.score),
+              margin: EdgeInsets.only(right: 8)),
+          Container(
+              child: Text(timeago.format(article.time)),
+              margin: EdgeInsets.only(right: 8)),
+          getReadableUrlWidget(article.url),
+        ]),
       ),
-
       trailing: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Icon(Icons.message_outlined),
-          Text(article.descendants.toString())
-        ]
-      ),
-
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.message_outlined),
+            Text(article.descendants.toString())
+          ]),
       onTap: () => Navigator.of(context)
           .push(MaterialPageRoute<void>(builder: (BuildContext context) {
         return ArticlePage(article: article);
@@ -63,76 +60,137 @@ class _FrontPageState extends State<FrontPage> {
   }
 
   Widget _buildArticlesList() {
-    return FutureBuilder<List<ArticleModel>>(
+    return FutureBuilder(
       future: futureArticles,
       builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return ListView.separated(
-            padding: EdgeInsets.all(15.0),
-            itemCount: snapshot.data.length,
-            separatorBuilder: (BuildContext context, int index) => Divider(height: 1),
-            itemBuilder: (context, i) {
-              return _buildRow(snapshot.data[i]);
-            }
-          );
-        } else if (snapshot.hasError) {
-          return getGenericErrorWidget(context);
-        }
+        // Detemine whether to render the loading/error state or the list
+        final int childCount = snapshot.hasData ? snapshot.data.length : 1;
 
-        // By default, show a loading spinner.
-        return Center(
-          child: CircularProgressIndicator()
+        return SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (ctx, i) {
+              if (snapshot.hasError) {
+                return getGenericErrorWidget(context);
+              }
+              if (snapshot.hasData == false || snapshot.hasData == null) {
+                return Center(child: CircularProgressIndicator());
+              }
+
+              return Column(children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                  child: _buildRow(snapshot.data[i])
+                ),
+                Separator()
+              ]);
+            },
+            childCount: childCount,
+          ),
         );
+
+        // if (snapshot.hasData) {
+        // return ListView.separated(
+        //     padding: EdgeInsets.all(15.0),
+        //     itemCount: snapshot.data.length,
+        //     separatorBuilder: (BuildContext context, int index) =>
+        //         Divider(height: 1),
+        //     itemBuilder: (context, i) {
+        //       return _buildRow(snapshot.data[i]);
+        //     });
+        // } else if (snapshot.hasError) {
+        //   return getGenericErrorWidget(context);
+        // }
+
+        // // By default, show a loading spinner.
+        // return Center(child: CircularProgressIndicator());
       },
     );
   }
 
-  List<Widget> _buildFrontPageHeader(
-      BuildContext context, bool innerBoxIsScrolled) {
-    return <Widget>[
-      SliverAppBar(
-        expandedHeight: 125.0,
-        floating: false,
-        pinned: true,
-        flexibleSpace: FlexibleSpaceBar(
-            centerTitle: true,
-            title: Text("The Index",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24.0,
-                shadows: [Shadow(color: Colors.black, blurRadius: 2)]
-              )
+  SliverAppBar _buildAppHeader(BuildContext context) {
+    return SliverAppBar(
+      pinned: true,
+      // leading: IconButton(icon: Icon(Icons.settings)),
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      expandedHeight: 200,
+      flexibleSpace: CustomizableSpaceBar(
+        builder: (context, scrollingRate) {
+          var collapsedOpacity = 1 - (scrollingRate * 1.25);
+          if (collapsedOpacity < 0) collapsedOpacity = 0;
+
+          // Render the collapsed state, containing just the app title
+          if (collapsedOpacity <= 0.15) {
+            double calculatedSlowedOpacity = 1 - (collapsedOpacity * 25);
+            if (calculatedSlowedOpacity < 0) calculatedSlowedOpacity = 0;
+            return AnimatedOpacity(
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeIn,
+                opacity: calculatedSlowedOpacity,
+                child: Padding(
+                    padding: EdgeInsets.only(left: 20, bottom: 15),
+                    // padding: EdgeInsets.only(bottom: 13, left: 52),
+                    child: Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Text(
+                        "Index _",
+                        style: TextStyle(
+                            fontSize: 42 - 18 * scrollingRate,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )));
+          }
+
+          // Render the opened state
+          return Opacity(
+            opacity: collapsedOpacity,
+            child: Card(
+              elevation: 8,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+
+                  SizedBox(height: 25),
+                  Text('The Index',
+                      style: TextStyle(
+                          // color: Colors.white,
+                          fontSize: 48.0)),
+                  SizedBox(height: 10),
+                  Text('Last updated at 17:45'),
+                  // Row(
+                  //   mainAxisSize: MainAxisSize.max,
+                  //   mainAxisAlignment: MainAxisAlignment.center,
+                  //   children: <Widget>[
+                  //     TextButton(
+                  //       child: Text('SETTINGS'),
+                  //       onPressed: () => {},
+                  //     ),
+                  //     SizedBox(width: 16),
+                  //     TextButton(
+                  //       child: Text('REFRESH'),
+                  //       onPressed: () => {},
+                  //     ),
+                  //     SizedBox(width: 16),
+                  //     TextButton(
+                  //       child: Text('REFRESH'),
+                  //       onPressed: () => {},
+                  //     ),
+                  //   ],
+                  // ),
+                ],
+              ),
             ),
-            background: Stack(fit: StackFit.expand, children: [
-              Image.network(
-                'https://images.pexels.com/photos/5699665/pexels-photo-5699665.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
-                fit: BoxFit.cover,
-              ),
-              const DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment(0.0, 0.5),
-                    end: Alignment(0.0, 0.0),
-                    colors: <Color>[
-                      Color(0x60000000),
-                      Color(0x00000000),
-                    ],
-                  ),
-                ),
-              ),
-            ])),
+          );
+        },
       ),
-    ];
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) =>
-            _buildFrontPageHeader(context, innerBoxIsScrolled),
-        body: _buildArticlesList(),
-      )
-    );
+        body: CustomScrollView(
+      slivers: [_buildAppHeader(context), SliverPadding(padding: EdgeInsets.all(10)), _buildArticlesList()],
+    ));
   }
 }
